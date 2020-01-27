7d4fc4b0c05b96d124be570f65de4a1b42573c58
chigozieorunta <chigozieorunta@yahoo.com>
2020-01-27 16:35:23 +0100

    Modified plugin, added functionality for filter, create event and post meta methods

--- woo-create-product-on-image-upload.php
+++ woo-create-product-on-image-upload.php
@@ -36,21 +36,52 @@ class wooCreateProductOnImageUpload {
 	 * @since  1.0.0
 	 */
     public static function createProductOnImageUpload() {
-		$user = wp_get_current_user();
-		if(isset($user->roles[0]) && $user->roles[0] == 'vendor') {
-			add_action('admin_enqueue_scripts', array(get_called_class(), 'registerScripts'));
-		}
-    }
-
-    /**
-	 * Register Scripts Method
+		add_filter('wp_handle_upload', 'customUploadFilter');
+	}
+	
+	/**
+	 * Load only when pluggable.php is ready
 	 *
      * @access public 
 	 * @since  1.0.0
 	 */
-    public static function registerScripts() {
-		wp_register_style('woo-create-product-on-image-upload', plugin_dir_url(__FILE__).'css/woo-create-product-on-image-upload.css');
-		wp_enqueue_style('woo-create-product-on-image-upload');
+    public static function customUploadFilter($file) {
+		$product = array(
+			'post_title' => $file['name'],
+			'post_content' => '',
+			'post_status' => 'publish',
+			'post_type' => "product",
+		);
+
+		//Create Product
+		$post_id = wp_insert_post($product);
+
+		//Set Terms
+		wp_set_object_terms($post_id, 'simple', 'product_type');
+
+		//Set Post Meta values...
+		update_post_meta( $post_id, '_visibility', 'visible' );
+		update_post_meta( $post_id, '_stock_status', 'instock');
+		update_post_meta( $post_id, 'total_sales', '0' );
+		update_post_meta( $post_id, '_downloadable', 'no' );
+		update_post_meta( $post_id, '_virtual', 'yes' );
+		update_post_meta( $post_id, '_regular_price', '' );
+		update_post_meta( $post_id, '_sale_price', '' );
+		update_post_meta( $post_id, '_purchase_note', '' );
+		update_post_meta( $post_id, '_featured', 'no' );
+		update_post_meta( $post_id, '_weight', '' );
+		update_post_meta( $post_id, '_length', '' );
+		update_post_meta( $post_id, '_width', '' );
+		update_post_meta( $post_id, '_height', '' );
+		update_post_meta( $post_id, '_sku', '' );
+		update_post_meta( $post_id, '_product_attributes', array() );
+		update_post_meta( $post_id, '_sale_price_dates_from', '' );
+		update_post_meta( $post_id, '_sale_price_dates_to', '' );
+		update_post_meta( $post_id, '_price', '' );
+		update_post_meta( $post_id, '_sold_individually', '' );
+		update_post_meta( $post_id, '_manage_stock', 'no' );
+		update_post_meta( $post_id, '_backorders', 'no' );
+		update_post_meta( $post_id, '_stock', '' );
     }
 
     /**
